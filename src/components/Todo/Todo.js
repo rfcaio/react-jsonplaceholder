dfe6eb479d420d412d55e2d3a4fd62f76650db2d
Caio Rodrigues <rfcaio90@gmail.com>
2020-01-21 14:37:26 -0300

    create a route for update todos

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { Route, Switch, useRouteMatch } from 'react-router-dom'
 
 import TodoList from './List'
+import TodoUpdate from './Update'
 
 const Todo = () => {
   const match = useRouteMatch()
   return (
     <Switch data-testid="todo">
+      <Route path={`${match.path}/update/:id`}>
+        <TodoUpdate />
+      </Route>
       <Route path={match.path}>
         <TodoList />
       </Route>
