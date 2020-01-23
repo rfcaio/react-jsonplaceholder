c604d980d051fd6e0c3b4a3a4e3ad683d136a1bf
Caio Rodrigues <rfcaio90@gmail.com>
2020-01-23 14:40:14 -0300

    fetch todo in `TodoUpdate` component

--- src/components/Todo/Update/TodoUpdate.js
+++ src/components/Todo/Update/TodoUpdate.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import { Card } from 'antd'
+
+import useTodo from './useTodo'
 
 const TodoUpdate = () => {
   const { id } = useParams()
+  const { error, loading, todo } = useTodo(id)
+
   return (
-    <p>Updating todo with ID {id}.</p>
+    <Card data-testid="todo-update" loading={loading} title="Update todo">
+      {error && <p>Could not fetch todo with ID {id}.</p>}
+      {todo && <h1>{todo.title}</h1>}
+    </Card>
   )
 }
 
--- src/components/Todo/Update/TodoUpdate.test.js
+++ src/components/Todo/Update/TodoUpdate.test.js
@@ -4,15 +4,49 @@ import { render } from '@testing-library/react'
 
 import TodoUpdate from './TodoUpdate'
 
+import useTodo from './useTodo'
+
 jest.mock('react-router')
+jest.mock('./useTodo')
 
 describe('TodoUpdate', () => {
+  beforeEach(() => {
+    useParams.mockReturnValue({ id: 7 })
+    useTodo.mockReturnValue({ error: false, loading: false, todo: null })
+  })
+
   describe('on render', () => {
-    test('show a message with todo id', () => {
-      useParams.mockReturnValue({ id: 7 })
+    test('show `Update todo` title', () => {
       const { getByText } = render(<TodoUpdate />)
-      expect(useParams).toHaveBeenCalled()
-      expect(getByText('Updating todo with ID 7.')).toBeInTheDocument()
+      expect(getByText('Update todo')).toBeInTheDocument()
+    })
+
+    test('show `Could not fetch todo with ID 7.` when a request error occur', () => {
+      useTodo.mockReturnValue({ error: true, loading: false, todo: null })
+      const { getByText } = render(<TodoUpdate />)
+      expect(getByText('Could not fetch todo with ID 7.')).toBeInTheDocument()
+    })
+
+    test('show loading indicator', () => {
+      useTodo.mockReturnValue({ error: false, loading: true, todo: null })
+      const { getByTestId } = render(<TodoUpdate />)
+      expect(getByTestId('todo-update')).toHaveClass('ant-card-loading')
+    })
+
+    test('show todo title', () => {
+      useTodo.mockReturnValue({
+        error: false,
+        loading: false,
+        todo: {
+          completed: false,
+          id: 7,
+          title: 'delectus aut autem',
+          userId: 7
+        }
+      })
+      const { getByText } = render(<TodoUpdate />)
+      expect(useTodo).toHaveBeenCalledWith(7)
+      expect(getByText('delectus aut autem')).toBeInTheDocument()
     })
   })
 })
