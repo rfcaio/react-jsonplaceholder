02b584b066e5061585e1ebf9d654317a1131dd2a
Caio Rodrigues <rfcaio90@gmail.com>
2020-02-01 00:05:06 -0300

    improve `useTodo` tests

--- src/hooks/useTodo.test.js
+++ src/hooks/useTodo.test.js
@@ -1,38 +1,40 @@
 import axios from 'axios'
-import { act, renderHook } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react-hooks'
 
 import useTodo from './useTodo'
 
 jest.mock('axios')
 
 describe('useTodo', () => {
-  test('request `http://jsonplaceholder.typicode.com/todos/1`', async () => {
-    axios.get.mockResolvedValue({ data: {} })
-    const { result, waitForNextUpdate } = renderHook(() => useTodo(1))
-    await waitForNextUpdate()
-    expect(axios.get)
-      .toHaveBeenCalledWith('http://jsonplaceholder.typicode.com/todos/1')
+  const mockedTodo = {
+    completed: false,
+    id: 1,
+    title: 'delectus aut autem',
+    userId: 1
+  }
+
+  beforeEach(() => {
+    axios
+      .get
+      .mockName('axios.get')
+      .mockImplementation(url => (
+        url === 'http://jsonplaceholder.typicode.com/todos/1'
+          ? Promise.resolve({ data: mockedTodo })
+          : Promise.reject(new Error())
+      ))
   })
 
-  test('get a single todo', async () => {
-    const data = {
-      completed: false,
-      id: 1,
-      title: 'delectus aut autem',
-      userId: 1
-    }
-    axios.get.mockResolvedValue({ data })
+  test('get todo by ID', async () => {
     const { result, waitForNextUpdate } = renderHook(() => useTodo(1))
     await waitForNextUpdate()
     const [todo, loading, error] = result.current
     expect(error).toBeFalsy()
     expect(loading).toBeFalsy()
-    expect(todo).toEqual(data)
+    expect(todo).toEqual(mockedTodo)
   })
 
-  test('set `error` to `true` when a request error occur', async () => {
-    axios.get.mockRejectedValue(new Error())
-    const { result, waitForNextUpdate } = renderHook(() => useTodo(1))
+  test('set `error` to `true` when an error occur', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useTodo(2))
     await waitForNextUpdate()
     const [todo, loading, error] = result.current
     expect(error).toBeTruthy()
