0bc75feeb4a1d159356c5ea732bb841462ba6ee0
Caio Rodrigues <rfcaio90@gmail.com>
2020-02-01 00:14:58 -0300

    improve `useUsers` tests

--- src/hooks/useUsers.test.js
+++ src/hooks/useUsers.test.js
@@ -1,32 +1,31 @@
 import axios from 'axios'
-import { act, renderHook } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react-hooks'
 
 import useUsers from './useUsers'
 
 jest.mock('axios')
 
 describe('useUsers', () => {
-  test('request `http://jsonplaceholder.typicode.com/users`', async () => {
-    axios.get.mockResolvedValue({ data: {} })
-    const { result, waitForNextUpdate } = renderHook(() => useUsers())
-    await waitForNextUpdate()
-    expect(axios.get)
-      .toHaveBeenCalledWith('http://jsonplaceholder.typicode.com/users')
+  const mockedUsers = [
+    { id: 1, name: 'Foo' },
+    { id: 2, name: 'Bar' }
+  ]
+
+  beforeEach(() => {
+    axios.get.mockName('axios.get').mockResolvedValue({ data: mockedUsers })
   })
 
-  test('get a users list', async () => {
-    const data = [{ id: 1, name: 'Foo' }, { id: 2, name: 'Bar' }]
-    axios.get.mockResolvedValue({ data })
+  test('get all users', async () => {
     const { result, waitForNextUpdate } = renderHook(() => useUsers())
     await waitForNextUpdate()
     const [users, loading, error] = result.current
     expect(error).toBeFalsy()
     expect(loading).toBeFalsy()
-    expect(users).toEqual(data)
+    expect(users).toEqual(mockedUsers)
   })
 
-  test('set `error` to `true` when a request error occur', async () => {
-    axios.get.mockRejectedValue(new Error())
+  test('set `error` to `true` when an error occur', async () => {
+    axios.get.mockName('axios.get').mockRejectedValue(new Error())
     const { result, waitForNextUpdate } = renderHook(() => useUsers())
     await waitForNextUpdate()
     const [users, loading, error] = result.current
