e5bf1d8ce4ef367fcdcbcb70f7609e8e042dc5dd
Caio Rodrigues <rfcaio90@gmail.com>
2020-02-03 13:40:24 -0300

    improve `useUsers` tests

--- src/hooks/useTodos.test.js
+++ src/hooks/useTodos.test.js
@@ -1,37 +1,35 @@
 import axios from 'axios'
-import { act, renderHook } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react-hooks'
 
 import useTodos from './useTodos'
 
 jest.mock('axios')
 
 describe('useTodos', () => {
-  test('request `http://jsonplaceholder.typicode.com/todos`', async () => {
-    axios.get.mockResolvedValue({ data: {} })
-    const { result, waitForNextUpdate } = renderHook(() => useTodos())
-    await waitForNextUpdate()
-    expect(axios.get)
-      .toHaveBeenCalledWith('http://jsonplaceholder.typicode.com/todos')
+  const mockedTodos = [{ id: 1, title: 'Foo' }, { id: 2, title: 'Bar' }]
+
+  beforeEach(() => {
+    axios.get.mockName('axios.get').mockResolvedValue({ data: mockedTodos })
   })
 
-  test('get a todos list', async () => {
-    const data = [{ id: 1, title: 'Foo' }, { id: 2, title: 'Bar' }]
-    axios.get.mockResolvedValue({ data })
+  test('get all todos', async () => {
     const { result, waitForNextUpdate } = renderHook(() => useTodos())
     await waitForNextUpdate()
     const [todos, loading, error] = result.current
     expect(error).toBeFalsy()
     expect(loading).toBeFalsy()
-    expect(todos).toEqual(data)
+    expect(todos).toEqual(mockedTodos)
   })
 
   test('set `error` to `true` when a request error occur', async () => {
-    axios.get.mockRejectedValue(new Error())
+    axios.get.mockName('axios.get').mockRejectedValue(new Error())
     const { result, waitForNextUpdate } = renderHook(() => useTodos())
     await waitForNextUpdate()
-    const [users, loading, error] = result.current
+    const [todos, loading, error] = result.current
     expect(error).toBeTruthy()
     expect(loading).toBeFalsy()
-    expect(users).toEqual([])
+    expect(todos).toEqual([])
   })
+
+  test.todo('delete todo by ID')
 })
