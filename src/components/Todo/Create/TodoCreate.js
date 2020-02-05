e686fb1bdbcb1afb5078c0a2131d0c09ed78cc3f
Caio Rodrigues <rfcaio90@gmail.com>
2020-02-05 13:56:14 -0300

    add create todo function

--- src/components/Todo/Create/TodoCreate.js
+++ src/components/Todo/Create/TodoCreate.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router'
+import { message } from 'antd'
+import axios from 'axios'
 
 import TodoForm from '../Form'
 
 const TodoCreate = () => {
+  const history = useHistory()
+  const [loading, setLoading] = useState(false)
+
+  const createTodo = data => {
+    setLoading(true)
+    axios
+      .post('http://jsonplaceholder.typicode.com/todos', data)
+      .then(() => {
+        message.success('Todo created with success.', 5)
+        history.push('/todo')
+      })
+      .catch(() => {
+        message.error('Could not create todo.', 5)
+        setLoading(false)
+      })
+  }
+
   return (
     <TodoForm
+      loading={loading}
       title="Create todo"
-      onSubmit={() => null}
+      onSubmit={createTodo}
     />
   )
 }
--- src/components/Todo/Create/TodoCreate.test.js
+++ src/components/Todo/Create/TodoCreate.test.js
@@ -3,6 +3,9 @@ import { render } from '@testing-library/react'
 
 import TodoCreate from './TodoCreate'
 
+// TODO: to check, without this an error occur
+jest.mock('react-router')
+
 describe('TodoCreate', () => {
   describe('on render', () => {
     test('show `TodoForm` component', () => {
@@ -14,5 +17,13 @@ describe('TodoCreate', () => {
       const { getByText } = render(<TodoCreate />)
       expect(getByText('Create todo')).toBeInTheDocument()
     })
+  })
+
+  describe('onSubmit', () => {
+    test.todo('show `Could not create todo.` when an error occur')
+
+    test.todo('show `Todo created with success.` when no error occur')
+
+    test.todo('redirect to `/todo`')
   })
 })
